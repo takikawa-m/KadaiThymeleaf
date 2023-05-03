e47b7ddda0a61b7b55e1af5ca2cc5d547982b15a
takikawa <takikawa@PC-TAKIKAWA-3.intranet.impressgroup.jp>
2023-05-03 17:44:49 +0900

    syuse

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,7 +3,6 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class InputController {
 
